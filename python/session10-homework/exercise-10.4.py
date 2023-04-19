2f00739c87cb7e8801edc6956769b2ea8904cca1
senker <senkerplov@gmail.com>
2023-04-19 18:41:46 +0200

    📝 notes

--- python/session10-homework/exercise-10.4.py
+++ python/session10-homework/exercise-10.4.py
@@ -5,3 +5,6 @@ gtg_sales = [('Coffee', 2018, 525.05),
              ('Banana', 2022, 524.08)]
 sorted_sales = sorted(gtg_sales, key=lambda x: x[1])
 print(sorted_sales)
+
+''' In the code above, key=lambda x: x[1] specifies that we want to sort 
+    the gtg_sales list based on the second element of each tuple, which is the year. '''
